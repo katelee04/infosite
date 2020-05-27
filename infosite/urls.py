4707f084696faf75c0aff299aefbd38fa8a6024e
Kaiwen <kwyyyu@gmail.com>
2020-05-27 19:50:05 -0400

    structure construction

--- infosite/urls.py
+++ infosite/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path
+from user import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^publication/$', views.publication, name='publication'),
+    url(r'^service/$', views.service, name='service'),
 ]
